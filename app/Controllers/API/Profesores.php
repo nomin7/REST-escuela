16d2b10bb6d22c1682e20362cd668017cb7d5dd2
Benjamin Fernandez <benfernandez.dev@gmail.com>
2020-11-20 13:31:59 -0600

    Primer Commit: Entorno de desarrollo, modelo Profesor y testeo de ruta GET

---
+++ app/Controllers/API/Profesores.php
@@ -0,0 +1,14 @@
+<?php namespace App\Controllers\API;
+
+use CodeIgniter\RESTful\ResourceController;
+
+class Profesores extends ResourceController
+{
+	public function index()
+	{
+		echo "hola";
+	}
+
+	//--------------------------------------------------------------------
+
+}
